0a3da9231ec5f84d387a049200e2774ba6b573ff
arielcami <71854140+arielcami@users.noreply.github.com>
2023-04-29 14:18:02 -0700

    funciones

---
+++ js/interactions.js
@@ -0,0 +1,13 @@
+function go(sel) {
+
+  if (sel == 1) {
+    window.location.href = "Acceso.html";
+  }
+  else if (sel == 2) {
+    window.location.href = "Registro.html";
+  }
+  else if (sel == 3) {
+    window.location.href = "Menu.html";
+  }
+
+}
