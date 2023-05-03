3267ccb99266f7cf277507035d28ce0daa5c6a87
arielcami <71854140+arielcami@users.noreply.github.com>
2023-05-02 19:39:52 -0700

    Nueva pagina

---
+++ js/interactions.js
@@ -0,0 +1,15 @@
+function go(sel) {
+
+  if (sel == 1) {
+    window.location.href = "Acceso.html";
+  }
+  else if (sel == 2) {
+    window.location.href = "Registro.html";
+  }
+  else if (sel == 3) {
+    window.location.href = "Ok.html";
+  }
+  else if (sel == 4) {
+    window.location.href = "Menu.html";
+  }
+}
